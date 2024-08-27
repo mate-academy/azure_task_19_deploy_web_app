0fa25a3beef0ee3ac712b1bbe0246b5dc9d816f5
oleksandrhaupt <o.haupt@steel.mil.ua>
2024-08-27 20:01:20 +0300

    update

--- task.ps1
+++ task.ps1
@@ -18,6 +18,7 @@ $appName = "mate-webapp"
 $planName = "mate-appservice-plan"
 $registryName = "task-19-todoapp"
 $containerImageName = "$acrLoginServer/todoapp:v1"
+$acrLoginServer = "$acrName.azurecr.io"
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -40,16 +41,20 @@ New-AzContainerRegistry -ResourceGroupName $resourceGroupName -Name $acrName -Sk
 Write-Host "Log in to registry ..."
 #Connect-AzContainerRegistry -Name $acrName
 az acr login --name $acrName --expose-token
-
+$TOKEN= az acr login --name $acrName --expose-token --output tsv --query accessToken
+$TOKEN | docker login $Registry --username $Username --password-stdin
+echo $TOKEN
+#Connect-AzContainerRegistry -Name $acrName -ExposeToken
 # Clone the repository
 cd ./app  # Navigate to the app directory
-
+#sudo docker login
 # Build the Docker image
 sudo docker build -t todoapp:v1 .
 
-$acrLoginServer = "$acrName.azurecr.io"
 
+#sudo docker login $acrLoginServer
 # Tag the Docker image with the full name
+
 sudo docker tag todoapp:v1 $acrLoginServer/todoapp:v1
 # connect container registry
 #Connect-AzContainerRegistry -Name $acrName
@@ -61,58 +66,58 @@ sudo docker tag todoapp:v1 $acrLoginServer/todoapp:v1
 # Push the Docker image to ACR and wait for it to finish
 Start-Process -FilePath "sudo" -ArgumentList "docker push $acrLoginServer/todoapp:v1" -Wait
 
-# Pause the script
-Read-Host -Prompt "Press Enter to continue..."
+# # Pause the script
+# Read-Host -Prompt "Press Enter to continue..."
 
-#Write-Host "Creating App Service plan $planName ..."
-#New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $planName -Location $location -Tier Free -NumberOfWorkers 1 -Linux
+# #Write-Host "Creating App Service plan $planName ..."
+# #New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $planName -Location $location -Tier Free -NumberOfWorkers 1 -Linux
 
-#Write-Host "Creating Web App $appName ..."
-#New-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -Location $location -AppServicePlan $planName -ContainerImageName "$acrLoginServer/todoapp:v1"
+# #Write-Host "Creating Web App $appName ..."
+# #New-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -Location $location -AppServicePlan $planName -ContainerImageName "$acrLoginServer/todoapp:v1"
 
-# Assuming the previous steps were successful, and you have the necessary variables set up:
-Write-Host "Creating Web App $appName as a container-based app..."
+# # Assuming the previous steps were successful, and you have the necessary variables set up:
+# Write-Host "Creating Web App $appName as a container-based app..."
 
-# Create the App Service Plan (if not already created)
-$plan = Get-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $planName
-if (-not $plan) {
-    Write-Host "Creating App Service Plan $planName..."
-    New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $planName -Location $location -Tier Free -NumberOfWorkers 1 -Linux
-}
+# # Create the App Service Plan (if not already created)
+# $plan = Get-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $planName
+# if (-not $plan) {
+#     Write-Host "Creating App Service Plan $planName..."
+#     New-AzAppServicePlan -ResourceGroupName $resourceGroupName -Name $planName -Location $location -Tier Free -NumberOfWorkers 1 -Linux
+# }
 
-# Create the Web App with container settings
-$webApp = New-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -Location $location -AppServicePlan $planName -ContainerImageName "DOCKER|$containerImageName"
+# # Create the Web App with container settings
+# $webApp = New-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -Location $location -AppServicePlan $planName -ContainerImageName $containerImageName
 
-# Configure the Web App to use ACR credentials
-Write-Host "Configuring Web App $appName to use ACR credentials ..."
-#add new part
-#Get-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName | Select-Object -ExpandProperty SiteConfig
-$acrCredentials = az acr credential show --name $acrName --resource-group $resourceGroupName | ConvertFrom-Json
-$acrUsername = $acrCredentials.username
-$acrPassword = $acrCredentials.passwords[0].value
+# # Configure the Web App to use ACR credentials
+# Write-Host "Configuring Web App $appName to use ACR credentials ..."
+# #add new partContainerImageName
+# #Get-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName | Select-Object -ExpandProperty SiteConfig
+# $acrCredentials = az acr credential show --name $acrName --resource-group $resourceGroupName | ConvertFrom-Json
+# $acrUsername = $acrCredentials.username
+# $acrPassword = $acrCredentials.passwords[0].value
 
-#$acrCredentials = (az acr credential show --name $acrName --resource-group $resourceGroupName --debug | ConvertFrom-Json)
-# Configure the Web App to use a Docker container
-#$containerConfig = New-Object Microsoft.Azure.Commands.WebApps.Models.WebAppContainer
-#$containerConfig.ContainerRegistryServerUrl = "https://$acrLoginServer"
-#$containerConfig.ContainerRegistryImageName = "$acrLoginServer/todoapp:v1"
+# #$acrCredentials = (az acr credential show --name $acrName --resource-group $resourceGroupName --debug | ConvertFrom-Json)
+# # Configure the Web App to use a Docker container
+# #$containerConfig = New-Object Microsoft.Azure.Commands.WebApps.Models.WebAppContainer
+# #$containerConfig.ContainerRegistryServerUrl = "https://$acrLoginServer"
+# #$containerConfig.ContainerRegistryImageName = "$acrLoginServer/todoapp:v1"
 
-#az acr credential show
+# #az acr credential show
 
-#$acrUsername = $acrCredentials.username
-#$acrPassword = $acrCredentials.passwords[0].value
-Set-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -AppSettings @{
-    "DOCKER_REGISTRY_SERVER_URL" = "https://$acrName.azurecr.io"
-    "DOCKER_REGISTRY_SERVER_USERNAME" = $acrUsername
-    "DOCKER_REGISTRY_SERVER_PASSWORD" = $acrPassword
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"  # Optional: Set to false if not needed
-    "DOCKER_ENABLE_CI" = "true"  # Optional: Continuous deployment
-}
+# #$acrUsername = $acrCredentials.username
+# #$acrPassword = $acrCredentials.passwords[0].value
+# Set-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName -AppSettings @{
+#     "DOCKER_REGISTRY_SERVER_URL" = "https://$acrName.azurecr.io"
+#     "DOCKER_REGISTRY_SERVER_USERNAME" = $acrUsername
+#     "DOCKER_REGISTRY_SERVER_PASSWORD" = $acrPassword
+#     "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"  # Optional: Set to false if not needed
+#     "DOCKER_ENABLE_CI" = "true"  # Optional: Continuous deployment
+# }
 
-Write-Host "Restarting Web App $appName to apply changes..."
-Restart-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName
-# Apply the container settings to the Web App
-#Set-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName -ContainerSettings $containerConfig
-#Restart-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName
+# Write-Host "Restarting Web App $appName to apply changes..."
+# Restart-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName
+# # Apply the container settings to the Web App
+# #Set-AzWebApp -ResourceGroupName $resourceGroupName -Name $webAppName -ContainerSettings $containerConfig
+# #Restart-AzWebApp -ResourceGroupName $resourceGroupName -Name $appName
 
-#az webapp config container set --name $appName --resource-group $resourceGroupName --docker-custom-image-name "$acrLoginServer/todoapp:v1" --docker-registry-server-url "https://$acrLoginServer" --docker-registry-server-user $acrUsername --docker-registry-server-password $acrPassword
+# #az webapp config container set --name $appName --resource-group $resourceGroupName --docker-custom-image-name "$acrLoginServer/todoapp:v1" --docker-registry-server-url "https://$acrLoginServer" --docker-registry-server-user $acrUsername --docker-registry-server-password $acrPassword
